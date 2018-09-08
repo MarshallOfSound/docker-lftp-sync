a004b0105a1c5187f76f8b6315718364eb9884a9
Samuel Attard <samuel.r.attard@gmail.com>
2018-09-09 00:27:02 +1000

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM ubuntu:18.04
+
+RUN apt update && apt install -y \
+    lftp \
+    cron \
+ && apt upgrade -y && rm -rf /var/lib/apt/lists/*
+
+ADD crontab /etc/cron.d/hello-cron
+ADD scripts /var/scripts
+
+RUN chmod 0644 /etc/cron.d/hello-cron
+
+RUN touch /var/log/cron.log
+
+RUN mkdir -p /media
+
+CMD cron && tail -f /var/log/cron.log
---
+++ scripts/sync.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+login=$LFTP_USER
+pass=$LFTP_PASSWORD
+host=$LFTP_HOST
+base_remote_dir=$LFTP_REMOTE_DIR
+base_local_dir=/media
+
+trap "rm -f /config/synctorrent.lock" SIGINT SIGTERM
+
+set -e
+
+if [ -e /config/synctorrent.lock ]
+then
+  echo "Sync is running already."
+  exit 1
+else
+  touch /config/synctorrent.lock
+  
+
+sync_dir() {
+
+remote_dir=$base_remote_dir/$1
+local_dir=$base_local_dir/$1
+
+echo "Syncing $remote_dir --> $local_dir"
+
+mkdir -p $local_dir
+
+lftp  << EOF
+set mirror:use-pget-n 2
+lftp -u $login,$pass $host
+mirror -c -P5 --no-perms --dereference $remote_dir $local_dir
+quit
+EOF
+
+}
+
+  echo "Sync Starting: $(date)"
+
+  old_ifs=$IFS
+  export IFS=";"
+  for dir in $LFTP_DIRS; do
+    export IFS=$old_ifs
+    echo "$dir"
+    export IFS=";"
+  done
+
+  echo "Sync Done: $(date)"
+
+  rm -f /config/synctorrent.lock
+  exit 0
+fi
